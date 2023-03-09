35c688a8a9fbbb74e91ddbeab12a8eb04ed105f4
dhauss <davidkhauss@gmail.com>
2023-03-09 11:03:23 -0500

    matrix constructors and toString

--- Midterm/Question1/complexmatrix/ComplexDriver.java
+++ Midterm/Question1/complexmatrix/ComplexDriver.java
@@ -4,6 +4,7 @@ package complexmatrix;
 public class ComplexDriver {
 	
 	public static void main(String[] args) {
+		/*
 		Complex a = new Complex();
 		Complex b = new Complex(2239);
 		Complex c = new Complex(5, 2);
@@ -12,12 +13,12 @@ public class ComplexDriver {
 		
 		System.out.println(a + "\n" + b + "\n" + c);
 		System.out.println(c.divide(a));
-
+		*/
 		
-		//CHECK ZERO DIVISION WITHIN DIVISION
-
-		
-		
+		ComplexMatrix a = new ComplexMatrix(3, 4);
+		Complex[][] b = {{new Complex(0, 0), new Complex(0, 1), new Complex(0, 2)}, {new Complex(1, 1),new Complex(1, 2), new Complex(1, 3)}};
+		ComplexMatrix c = new ComplexMatrix(b);
+		System.out.println(c);		
 	}
 
 }
--- Midterm/Question1/complexmatrix/ComplexMatrix.java
+++ Midterm/Question1/complexmatrix/ComplexMatrix.java
@@ -1,9 +1,85 @@
 package complexmatrix;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class ComplexMatrix {
+		private ArrayList<ArrayList<Complex>> matrix;
+		private int rows;
+		private int cols;
+		
+		//m = rows, n = cols
+		public ComplexMatrix(int m, int n) {
+			matrix = new ArrayList<ArrayList<Complex>>(m);
+			setRows(m);
+			setCols(n);
+			
+			ArrayList<Complex> init = new ArrayList<Complex>(n);
+			for(int i = 0; i < n; i++) {
+				init.add(new Complex());
+			}
+			
+			for(int i = 0; i < m; i++) {
+				matrix.add(init);
+			}
+			
+		}
+		
+		public ComplexMatrix(Complex[][] inArray) {
+			int m = inArray.length;
+			//assuming col lengths are all equal, otherwise I'd loop through and find the max length
+			int n = inArray[0].length;
+			setRows(m);
+			setCols(n);
+			
+			matrix = new ArrayList<ArrayList<Complex>>(m);
+			for(int i = 0; i < m; i++) {
+				ArrayList<Complex> in = new ArrayList<>(Arrays.asList(inArray[i]));
+				matrix.add(in);
+			}	
+		}
+		
+		@Override
+		public String toString() {
+			for(int i = 0; i < getRows(); i++) {
+				for(int j = 0; j < getCols(); j++) {
+					System.out.print(getMatrix().get(i).get(j) + " ");
+				}
+				System.out.println();
+			}
+			
+			
+			return "";
+		}
 	
-	
+		public ArrayList<ArrayList<Complex>> getMatrix() {
+			return matrix;
+		}
+
+		public void setMatrix(ArrayList<ArrayList<Complex>> matrix) {
+			this.matrix = matrix;
+		}
+		
+		
+
+	public int getRows() {
+			return rows;
+		}
+
+		public void setRows(int rows) {
+			this.rows = rows;
+		}
+
+	public int getCols() {
+			return cols;
+		}
+
+		public void setCols(int cols) {
+			this.cols = cols;
+		}
+
 	public static void main(String[] args) {
 
 	}
