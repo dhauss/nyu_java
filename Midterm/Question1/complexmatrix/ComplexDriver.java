19967eb1af99960528e340a6a82b6abd55c41b31
dhauss <davidkhauss@gmail.com>
2023-03-10 12:02:13 -0500

    finished

--- Midterm/Question1/complexmatrix/ComplexDriver.java
+++ Midterm/Question1/complexmatrix/ComplexDriver.java
@@ -4,21 +4,46 @@ package complexmatrix;
 public class ComplexDriver {
 	
 	public static void main(String[] args) {
-		/*
-		Complex a = new Complex();
-		Complex b = new Complex(2239);
-		Complex c = new Complex(5, 2);
-		Complex d = new Complex(3, 2);
+		
+		Complex a1 = new Complex();
+		Complex b1 = new Complex(2239);
+		Complex c1 = new Complex(5, 2);
+		Complex d1 = new Complex(3, 2);
 
 		
-		System.out.println(a + "\n" + b + "\n" + c);
-		System.out.println(c.divide(a));
-		*/
+		System.out.println(a1 + "\n" + b1 + "\n" + c1);
+		System.out.println(c1.divide(d1));
+		
 		
 		ComplexMatrix a = new ComplexMatrix(3, 4);
-		Complex[][] b = {{new Complex(0, 0), new Complex(0, 1), new Complex(0, 2)}, {new Complex(1, 1),new Complex(1, 2), new Complex(1, 3)}};
+		Complex[][] b = {
+				{new Complex(1, 2), new Complex(2, 2)},
+				{new Complex(1, 2), new Complex(2, 2)},
+				{new Complex(1, 2),new Complex(2, 2)}
+				};
+		Complex[][] b2 = {
+				{new Complex(2, -2), new Complex(2, 2), new Complex(2, 2)},
+				{new Complex(2, 2), new Complex(-2, 2), new Complex(2, 2)}
+				};	
 		ComplexMatrix c = new ComplexMatrix(b);
-		System.out.println(c);		
+		ComplexMatrix d = new ComplexMatrix(b2);
+		
+		
+		try {
+			System.out.println(c.mult(d));
+		} catch (MatrixDimensionException e) {
+			System.out.println(e.getMessage());
+		}	
+		
+		
+		d.write("test.txt");
+		
+		try {
+			System.out.println(ComplexMatrix.read("test.txt"));
+		} catch (IncompatibleMatrixException e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 
 }
--- Midterm/Question1/complexmatrix/ComplexMatrix.java
+++ Midterm/Question1/complexmatrix/ComplexMatrix.java
@@ -1,8 +1,13 @@
 package complexmatrix;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 
 public class ComplexMatrix {
@@ -10,7 +15,6 @@ public class ComplexMatrix {
 		private int rows;
 		private int cols;
 		
-		//m = rows, n = cols
 		public ComplexMatrix(int m, int n) {
 			matrix = new ArrayList<ArrayList<Complex>>(m);
 			setRows(m);
@@ -23,35 +27,154 @@ public class ComplexMatrix {
 			
 			for(int i = 0; i < m; i++) {
 				matrix.add(init);
-			}
-			
+			}	
 		}
 		
 		public ComplexMatrix(Complex[][] inArray) {
-			int m = inArray.length;
-			//assuming col lengths are all equal, otherwise I'd loop through and find the max length
-			int n = inArray[0].length;
-			setRows(m);
-			setCols(n);
+			//assuming legal input, e.g. col lengths are all equal
+			setRows(inArray.length);
+			setCols(inArray[0].length);
 			
-			matrix = new ArrayList<ArrayList<Complex>>(m);
-			for(int i = 0; i < m; i++) {
+			matrix = new ArrayList<ArrayList<Complex>>(getRows());
+			for(int i = 0; i < getRows(); i++) {
 				ArrayList<Complex> in = new ArrayList<>(Arrays.asList(inArray[i]));
 				matrix.add(in);
 			}	
 		}
 		
-		@Override
-		public String toString() {
+		public ComplexMatrix add(ComplexMatrix cm) throws MatrixDimensionException {
+			if(this.getRows() != cm.getRows() || this.getCols() != cm.getCols())
+				throw new MatrixDimensionException("Incompatible Matrix Dimensions for Addition");
+			
+			Complex[][] res = new Complex[this.getRows()][this.getCols()];
+			
 			for(int i = 0; i < getRows(); i++) {
 				for(int j = 0; j < getCols(); j++) {
-					System.out.print(getMatrix().get(i).get(j) + " ");
+					Complex cell = this.getMatrix().get(i).get(j).add(cm.getMatrix().get(i).get(j));
+					res[i][j] = cell;
+					}
 				}
-				System.out.println();
+			
+			return new ComplexMatrix(res);
+		}
+		
+		
+		public ComplexMatrix mult(ComplexMatrix cm) throws MatrixDimensionException {
+			if(this.getCols() != cm.getRows())
+				throw new MatrixDimensionException("Incompatible Matrix Dimensions for Multiplication");
+			
+			Complex[][] res = new Complex[this.getRows()][cm.getCols()];
+			
+			for(int i = 0; i < this.getRows(); i++) {
+				for(int j = 0; j < cm.getCols(); j++) {
+					res[i][j] = new Complex();
+					for(int k = 0; k < this.getCols(); k++) {
+						res[i][j] = res[i][j].add(this.getMatrix().get(i).get(k).multiply(cm.getMatrix().get(k).get(j)));
+					}
+				}
 			}
 			
+			return new ComplexMatrix(res);
+		}
+		
+		public void write(String filename) {
+			FileWriter fout = null;
+			BufferedWriter out;
+			try {
+				fout = new FileWriter(filename);
+			} catch (IOException e) {
+				System.err.println("Filewriter error: "+ e.getMessage());
+				System.exit(1);
+			}
+			out = new BufferedWriter(fout);
 			
-			return "";
+			for (ArrayList<Complex> cList: this.getMatrix()) {
+				try {
+					for(Complex c: cList) {
+						String temp = String.format("%.2f_%.2f ", c.getReal(), c.getImaginary());
+						out.write(temp);
+					}
+					out.newLine();
+					out.flush();
+				} catch (IOException e) {
+					System.err.println("Error writing to file: "+ e.getMessage());
+				}
+			}
+			try {
+				out.flush();
+				fout.close();
+			} catch (IOException e) {
+				System.err.println(e.getMessage());
+			}
+		}
+		
+		public static ComplexMatrix read(String filename) throws IncompatibleMatrixException {
+			FileReader fr = null;
+			BufferedReader br = null;
+			ArrayList<ArrayList<Complex>> matrix = new ArrayList<ArrayList<Complex>>();
+			Complex[][] arrMatrix;
+			int rows, cols;
+			
+			try {
+				fr = new FileReader(filename);
+			}
+			catch(FileNotFoundException fnfe) {
+				System.out.println("File not found: " + fnfe.getMessage());
+				System.exit(1);
+			}
+			br = new BufferedReader(fr);
+			
+			try {
+				String curLine = br.readLine();
+				int i = 0;
+				while(curLine != null) {
+					matrix.add(new ArrayList<Complex>());
+					curLine = curLine.trim();
+					String[] nums = curLine.split(" ");
+					for(String num: nums) {
+						String[] temp = num.split("_");
+						matrix.get(i).add(new Complex(Double.parseDouble(temp[0]), Double.parseDouble(temp[1])));
+					}
+					i++;
+					curLine = br.readLine();
+				}
+				fr.close();
+			}
+			catch(IOException ioe){
+				System.err.println("Error reading file: " + ioe.getMessage());
+			}
+			
+			rows = matrix.size();
+			cols = matrix.get(0).size();
+			arrMatrix = new Complex[rows][cols];
+			
+			for(ArrayList<Complex> c: matrix) {
+				if(c.size() != cols) {
+					throw new IncompatibleMatrixException("Incompatible Matrix Dimensions");
+				}
+			}
+			
+			for(int i = 0; i < rows; i++) {
+				for(int j = 0; j < cols; j++) {
+					arrMatrix[i][j] = matrix.get(i).get(j);
+				}
+			}
+			
+			return new ComplexMatrix(arrMatrix);
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder res = new StringBuilder("");
+			
+			for(int i = 0; i < getRows(); i++) {
+				for(int j = 0; j < getCols(); j++) {
+					res.append(getMatrix().get(i).get(j));
+					res.append(" ");
+				}
+				res.append("\n");
+			}
+			return res.toString();
 		}
 	
 		public ArrayList<ArrayList<Complex>> getMatrix() {
@@ -63,8 +186,7 @@ public class ComplexMatrix {
 		}
 		
 		
-
-	public int getRows() {
+		public int getRows() {
 			return rows;
 		}
 
@@ -72,7 +194,7 @@ public class ComplexMatrix {
 			this.rows = rows;
 		}
 
-	public int getCols() {
+		public int getCols() {
 			return cols;
 		}
 
