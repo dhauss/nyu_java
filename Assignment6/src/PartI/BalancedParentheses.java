414b169f34c281f89e17187369ec84d0ce92c9dc
dhauss <davidkhauss@gmail.com>
2023-03-26 20:20:40 -0400

    balanced parentheses implemented

--- Assignment6/src/PartI/BalancedParentheses.java
+++ Assignment6/src/PartI/BalancedParentheses.java
@@ -1,14 +1,44 @@
 package PartI;
 
+import java.util.Stack;
+
 public class BalancedParentheses {
 
 	public static boolean isBalanced(String inString) {
-		return false;
+		Stack<Character> parStack = new Stack<>();
+		
+		for(int i = 0; i < inString.length(); i++) {
+			switch(inString.charAt(i)) {
+				case '(':
+					parStack.push('(');
+					break;
+				case ')':
+					if(parStack.isEmpty()) {
+						return false;
+					}
+					parStack.pop();
+					break;
+				default:
+					continue;
+			}	
+		}
+		return parStack.isEmpty();
 	}
-	
+
 	public static void main(String[] args) {
 		boolean result = isBalanced("(()()()())");
+		System.out.println(result);
 		result = isBalanced("(((())))");
+		System.out.println(result);
 		result = isBalanced("((((((())");
+		System.out.println(result);
+		
+		result = isBalanced("");
+		System.out.println(result);
+		result = isBalanced("(((");
+		System.out.println(result);
+		result = isBalanced("))))");
+		System.out.println(result);
+
 	}
 }
