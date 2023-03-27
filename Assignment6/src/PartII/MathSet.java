6bad8e5b904cc5578244eb5ec2bf58183c0e357a
dhauss <davidkhauss@gmail.com>
2023-03-27 14:30:47 -0400

    implemented cartesianProduct and SortFrequency

--- Assignment6/src/PartII/MathSet.java
+++ Assignment6/src/PartII/MathSet.java
@@ -31,11 +31,19 @@ public class MathSet<E> extends HashSet<E> {
     }
 
 	public <T> Set<Pair<E,T>> cartesianProduct(Set<T> s2) {
-        return null;  
+		Set<Pair<E, T>> res = new HashSet<>();
+		
+		for(E e1: (Set<E>) super.clone()) {
+			for(T e2: s2) {
+				Pair<E, T> temp = new Pair<>(e1, e2);
+				res.add(temp);
+			}
+		}
+		
+        return res;  
 
     }
     
-
 	public static void main(String[] args) {
 		// create two MathSet objects of the same type.
 		// See if union and intersection works.
@@ -47,19 +55,23 @@ public class MathSet<E> extends HashSet<E> {
 		s1.add(5);
 		s1.add(7);
 		s1.add(9);
+		
 		MathSet<Integer> s2 = new MathSet<Integer>();
 		s2.add(5);
 		s2.add(7);
 		s2.add(4);
 		s2.add(6);
 		s2.add(8);
-		
+	
+		MathSet<String> s3 = new MathSet<>();
+		s3.add("cat");
+		s3.add("dog");
+
 		System.out.println(s1.intersection(s2));
-
 		System.out.println(s1.union(s2));
-
-		//System.out.println(s1.cartesianProduct(s2));
-		
-		
+		System.out.println(s1.cartesianProduct(s2));
+		System.out.println(s2.cartesianProduct(s1));
+		System.out.println(s1.cartesianProduct(s3));
+		System.out.println(s3.cartesianProduct(s1));
 	}
 }
--- Assignment6/src/PartIII/SortFrequency.java
+++ Assignment6/src/PartIII/SortFrequency.java
@@ -1,11 +1,51 @@
 package PartIII;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SortFrequency {
+	
+    /*
+     * An alternative method would be to create a new hashMap, reverseHash, with the
+     * countHash keys and values reversed, so that the reverseHash keys are the count and
+     * its values are the actual integers. We then sort a list of reverseHash keys
+     * and access reverseHash by iterating through this list in order to create our final,
+     * 'sorted' list. Whenever two integers from ar have the same count, however, this will lead 
+     * to hashMap entries being overwritten, thereby losing entries with duplicate 
+     * counts. By storing each entry in a list and overriding the comparator before
+     * sorting, we can avoid this issue and successfully sort the original ar by count
+     * even with duplicate counts
+     * */
 
     public static void sortByFrequency(ArrayList<Integer> ar) {
+            HashMap<Integer, Integer> countHash = new HashMap<>();
+            ArrayList<Map.Entry<Integer, Integer>> countArray = new ArrayList<>();
             
+            //create count hashMap
+            for(int num: ar) {
+            	if((countHash.containsKey(num))) {
+            		int count = countHash.get(num);
+            		count++;
+            		countHash.put(num, count);
+            	}
+            	else {
+            		countHash.put(num, 1);
+            	}
+            }
+                
+            //add countHash entries to arrayList, override comparator and sort by value (count)
+            countArray.addAll(countHash.entrySet());
+            countArray.sort((e1, e2) -> e1.getValue() - e2.getValue());
+
+            //populate original array with keys sorted and repeated by value (count) 
+            ar.clear();
+            for(int i = 0; i < countArray.size(); i++) {
+            	for(int j = 0; j < countArray.get(i).getValue(); j++) {
+            		ar.add(countArray.get(i).getKey());
+            	}
+            }
     }
     
     public static void main(String[] args) {
@@ -16,5 +56,6 @@ public class SortFrequency {
             System.out.println(ar.toString());
             sortByFrequency(ar);
             System.out.println(ar.toString());
+           
     }
 }
