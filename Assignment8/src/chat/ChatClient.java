e1623fd73a78f57e732337951e43b829bfead797
dhauss <davidkhauss@gmail.com>
2023-04-11 16:11:21 -0400

    GUI implemented, messages being passed from client to server

--- Assignment8/src/chat/ChatClient.java
+++ Assignment8/src/chat/ChatClient.java
@@ -1,23 +1,116 @@
 package chat;
 
-import javax.swing.*;
+import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
 
 
 public class ChatClient extends JFrame implements Runnable {
 
 	private static int WIDTH = 400;
 	private static int HEIGHT = 300;
+	private static int threadID = 1;
+	private int id;
+	private TextArea textArea;
+	private Socket socket;
+	private DataOutputStream toServer;
+	String host = "localhost";
+
 	
 	public ChatClient() {
 		super("Chat Client");
+		id = threadID++;
 		this.setSize(ChatClient.WIDTH, ChatClient.HEIGHT);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		createMenu();
+		textArea = new TextArea(5, 20);
+		textArea.addKeyListener(new EnterListener());
+		JScrollPane scroll = new JScrollPane(textArea);
+		this.add(scroll);
 		this.setVisible(true);
-		
 	}
 	
 	public void run() {
+	}
+	
+	private void createMenu() {
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menu = new JMenu("File");
+		JMenuItem connectItem = new JMenuItem("Connect");
+		menu.add(connectItem);
+		connectItem.addActionListener(new ConnectListener());
+		JMenuItem exitItem = new JMenuItem("Exit");
+		exitItem.addActionListener((e) -> System.exit(0));
+		menu.add(exitItem);
+		menuBar.add(menu);
+		this.setJMenuBar(menuBar);
+	}
+	
+	private class ConnectListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String IPAddress = null;
+			try {
+				IPAddress = InetAddress.getLocalHost().toString();
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+			}
+			String message = String.format(
+					"Client %d's host name is %s\nClient %d's IP Address is %s",
+					id, host, id, IPAddress
+					);			
+			try {
+				socket = new Socket(host, 9898);
+				toServer = new DataOutputStream(socket.getOutputStream());
+				toServer.writeUTF(message);
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+	
+	private class EnterListener implements KeyListener {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+				textArea.append(" ");	//fixes bug where sending a carriage return alone resends last line of text
+				String[] lines = textArea.getText().split("\n");
+				System.out.println(id + ": " + lines[lines.length - 1]);
+				String message = id + ": " + lines[lines.length - 1];
+				
+				try {
+					toServer = new DataOutputStream(socket.getOutputStream());
+					toServer.writeUTF(message);
+				} catch (UnknownHostException e1) {
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}	
+		}
+		
+		@Override
+		public void keyTyped(KeyEvent e) {}
 
+		@Override
+		public void keyReleased(KeyEvent e) {}
+	
 	}
 	
 	public static void main(String[] args) {
--- Assignment8/src/chat/ChatServer.java
+++ Assignment8/src/chat/ChatServer.java
@@ -1,21 +1,63 @@
 package chat;
 
 
-import javax.swing.*;
+import java.awt.TextArea;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.Instant;
 
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
 
 public class ChatServer extends JFrame implements Runnable {
 
 	private static int WIDTH = 400;
 	private static int HEIGHT = 300;
+	private TextArea textArea;
+	private ObjectOutputStream outputToBroadcast;
+	private DataInputStream inputFromClient;
+
 	
 	public ChatServer() {
 		super("Chat Server");
 		this.setSize(ChatServer.WIDTH, ChatServer.HEIGHT);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		createMenu();
+		textArea = new TextArea(5, 20);
+		textArea.setEditable(false);
+		JScrollPane scroll = new JScrollPane(textArea);
+		this.add(scroll);
 		this.setVisible(true);
-		
+		Instant instant = null;
+		ServerSocket serverSocket = null;
+
+	     try {
+			serverSocket = new ServerSocket(9898);
+			instant = Instant.now();
+		    textArea.append("Server started: " + instant);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	     
+	     while (true) {
+	    	 Socket socket;
+	         try {
+				socket = serverSocket.accept();			    
+				inputFromClient = new DataInputStream(socket.getInputStream());
+
+			    String in = inputFromClient.readUTF();
+			    System.out.println(in);
+			    textArea.append("\n" + in); 
+			 } catch (IOException e1) {
+				e1.printStackTrace();
+			}
+	       }
 	}
 	
 	private void createMenu() {
